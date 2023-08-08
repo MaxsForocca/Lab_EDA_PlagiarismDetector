7f5c76be1d88de6a89b16a7a0a9f05d5111e83fe
MaxsForocca <mforocca@unsa.edu.pe>
2023-08-07 22:22:22 -0500

    agregando tries y loadfiles

--- PlagiarismDetector/src/plagiarismdetector/PlagiarismDetector.java
+++ PlagiarismDetector/src/plagiarismdetector/PlagiarismDetector.java
@@ -1,20 +1,64 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package plagiarismdetector;
 
-/**
- *
- * @author ASUS TUF GAMMING F15
- */
-public class PlagiarismDetector {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+import Trie.Trie;
+
+public class PlagiarismDetector {
+    static String folderPath = "../textos"; //ruta de la carpeta
+
+    static File folder = new File(folderPath); // indicamos la carpeta de archivos
+    static File[] files = folder.listFiles(); // obtenemos los archivos de la carpeta
+    static ArrayList<Trie> tries= new ArrayList<>(); //array de tries
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+    }
+    public static boolean loadFiles(File[] files){
+        if (files != null) {
+            // iterador para el arraylist de Trie
+            int idxTries = 0;
+            // recorrer los archivos del arreglo
+            for (File file : files) {
+                if (file.isFile()) {
+                    // leer archivo y crear un nuevo Trie para ese archivo
+                    System.out.println("Leyendo archivo: " + file.getName());
+                    tries.add(new Trie());
+                    try {
+                        Scanner scanner = new Scanner(file);
+                        // recorrer cada linea del archivo 
+                        while (scanner.hasNextLine()) {
+                            String line = scanner.nextLine();
+                            // filtamos los caracteres especiales y lo volvemos en minusculas
+                            // cada palabra se queda guardado en un arreglo
+                            String[] words = line.toLowerCase().split("\\W+");
+                            // introducimos cada palabra al Trie correspondiente
+                            for(String word:words)
+                                tries.get(idxTries).insert(word);
+                            System.out.println(line);
+                        }
+                        scanner.close();
+                    } catch (FileNotFoundException e) {
+                        System.out.println(e);
+                    }
+                    System.out.println("--------------------");
+                }
+                // aumentamos el iterador
+                idxTries++;
+            }
+            return true;
+        } else {
+            System.out.println("La carpeta no existe o no contiene archivos.");
+            return false;
+        }
     }
     
+    public ResultChecker verifyPlagiarism(String path){
+        ResultChecker result = null;
+
+        return result;
+    }
 }
